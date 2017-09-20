8520a2fbd1caf99ae17bda95ec045daf84697c43
carlitos321 <ca.cabrera321@gmail.com>
2017-09-20 16:15:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 function shout(string) {
   return string.toUpperCase()
+  console.log()
 }
 
 function whisper(string) {
   return string.toLowerCase()
-}
-
-function logShout(string) {
-  return string.toUpperCase()
-  console.log()
 }
